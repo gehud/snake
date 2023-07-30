ef7c4baf98337f6bb10a05215a1fb3789d60bed9
Nikolay <grozdovnikolay@gmail.com>
2023-07-30 18:07:01 +0300

    Screen orientation lock

--- Assets/Snake/Plugins/Web.cs
+++ Assets/Snake/Plugins/Web.cs
@@ -3,34 +3,19 @@ using UnityEngine;
 
 namespace Snake {
 	public class Web : MonoBehaviour {
-		public static bool IsMobile() {
-			//#if !UNITY_EDITOR && UNITY_WEBGL
-			//					return IsMobileInternal();
-			//#else
-			//			return false;
-			//#endif
-			return false;
-		}
+		[DllImport("__Internal")]
+		private static extern void EnterFullscreenInternal();
 
-		//[DllImport("__Internal")]
-		//private static extern bool IsMobileInternal();
-
-		//[DllImport("__Internal")]
-		//private static extern void EnterFullscreenInternal();
-
-		//[DllImport("__Internal")]
-		//private static extern void LockPortraitInternal();
+		[DllImport("__Internal")]
+		private static extern void LockPortraitOrientationInternal();
 
 		private void Awake() {
-//#if !UNITY_EDITOR && UNITY_WEBGL
-//		if (IsMobile()) {
-//			EnterFullscreenInternal();
-//			LockPortraitInternal();
-//		}
-//#endif
+#if !UNITY_EDITOR && UNITY_WEBGL
 			if (Application.isMobilePlatform) {
-				Screen.orientation = ScreenOrientation.Portrait;
+				EnterFullscreenInternal();
+				LockPortraitOrientationInternal();
 			}
+#endif
 		}
 	}
 }
---
+++ Assets/Snake/Plugins/web.jslib
@@ -0,0 +1,12 @@
+mergeInto(LibraryManager.library, {
+    EnterFullscreenInternal: function() {
+        // let canvas = document.querySelector('canvas');
+        // if (canvas) {
+        //     canvas.requestFullscreen();
+        // }
+    },
+
+    LockPortraitOrientationInternal: function() {
+        // window.screen.orientation.lock('portrait-primary');
+    }
+});
